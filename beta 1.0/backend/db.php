c9acca568cc9af7911786bebe901b51212a75d3f
RxNexium <markricio@gmail.com>
2020-03-04 13:00:41 -0500

    Add files via upload
    - added background image
    - updated midend to call backend and check for specific authorization (instructor and student)
    - added comments for clarity

---
+++ beta 1.0/backend/db.php
@@ -0,0 +1,10 @@
+<?php
+
+define ('DB_HOST','************');
+define ('DB_USER','************');
+define ('DB_PASS','************');
+define ('DB_NAME','************');
+
+$dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
+$db = new PDO($dsn, DB_USER, DB_PASS);
+?>
