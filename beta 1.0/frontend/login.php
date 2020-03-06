0fd14530ca8bdf6245aecf71858065b84fbd1213
RxNexium <markricio@gmail.com>
2020-03-06 16:10:20 -0500

    Beta 1.1

--- beta 1.0/frontend/login.php
+++ beta 1.0/frontend/login.php
@@ -4,8 +4,16 @@
 	<script src="login_front.js" type="text/JavaScript"></script>
 </head>
 <body>
+<div id="bg">
+  <img src="images/background2.jpg" alt="background">
+</div>
+
 <div id="login_div">
 	<form id="login" name="login" method="post" class="form-login">
+	
+	<div class="loginbox">
+
+	<div class="disableBlur">
 		<h1>NJIT Online Exam</h1>
 		<p>Please log in using your UCID and password.</p>
 		<div class="dialog">
@@ -25,12 +33,16 @@
 					<input class="form-control" type="button" onclick="ajaxLoginFunction();" value="Login">
 				</div></td>
 				</tr>
+			</div>
 			</tbody>
 		</table>
 		</div>
+		</div>
+	</div>
 	</form>
-</div>
+
+
 <script type="text/javascript">  
-	var MID_PATH="https://web.njit.edu/~mtr22/CS490/alpha/midend/";
+	var MID_PATH="https://web.njit.edu/~mtr22/CS490/beta/midend/"; //changed midpath
 </script>
 <div id="ajaxDiv"></div>
