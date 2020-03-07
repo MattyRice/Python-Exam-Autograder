faab471f866174b6740d0801a29c38d41b619159
Matthew Ricio <markricio@gmail.com>
2020-03-06 21:11:43 -0500

    lol

--- beta 1.1/backend/db.php
+++ beta 1.1/backend/db.php
@@ -1,9 +1,11 @@
 <?php
+//database connection
 
-define ('DB_HOST','************');
-define ('DB_USER','************');
-define ('DB_PASS','************');
-define ('DB_NAME','************');
+	define ('DB_HOST','sql2.njit.edu');
+	define ('DB_USER','drk25');
+	define ('DB_PASS','tertiary5');
+
+define ('DB_NAME','drk25');
 
 $dsn = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
 $db = new PDO($dsn, DB_USER, DB_PASS);
